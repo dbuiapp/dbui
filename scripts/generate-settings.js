21e1361b6d5e22c77c3490a0391ba557aea118aa
Richard Hoffman <coverslide@gmail.com>
2017-01-23 23:03:35 -0800

    version based on package.json

--- scripts/generate-settings.js
+++ scripts/generate-settings.js
@@ -1,9 +1,10 @@
 const fs = require('fs-promise');
 
 (async  () => {
-  const version = await fs.readFile('VERSION', 'utf8');
+  const pkgFile = await fs.readFile('package.json', 'utf8');
+  const pkgSettings = JSON.parse(pkgFile);
   const settings = {
-    version
+    version: pkgSettings.version
   };
   await fs.writeFile('settings.json', JSON.stringify(settings, null, 4));
 })();
